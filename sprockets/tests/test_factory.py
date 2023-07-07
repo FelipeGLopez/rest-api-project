712f138c51ebf34d15552036bed676fa8450f617
Felipe López <fglf18@gmail.com>
2023-07-07 19:42:59 -0300

    Fix typo in test

--- sprockets/tests/test_factory.py
+++ sprockets/tests/test_factory.py
@@ -10,14 +10,14 @@ def test_get_factory_then_success(api_client):
         id=13,
         name="Test Factory 1",
     )
-    response = api_client.get(f"/factories/{factory_object.id}/", format="json") 
+    response = api_client.get(f"/factories/{factory_object.id}/", format="json")
     assert response.status_code == status.HTTP_200_OK
     assert response.data["name"] == factory_object.name
     assert response.data["id"] == factory_object.id
     assert Factory.objects.count() == 1
 
 
-@pytest.mark.django_db  
-def test_get_non_existent_sprocket_then_failure(api_client):
+@pytest.mark.django_db
+def test_get_non_existent_factory_then_failure(api_client):
     response = api_client.get("/factories/1/", format="json")
     assert response.status_code == status.HTTP_404_NOT_FOUND
