98542ac7cea1b7962183418fe99d414e2a0fdca8
Felipe López <fglf18@gmail.com>
2023-07-06 23:20:51 -0300

    Add models and migration

--- sprockets/models.py
+++ sprockets/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Factory(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Sprocket(models.Model):
+    outside_diameter = models.IntegerField(help_text="Sprocket diameter.")
+    pitch_diameter = models.IntegerField(
+        help_text="Pitch circle diameter of the sprocket."
+    )
+    pitch = models.IntegerField(
+        help_text="Distance between the centers of two adjacent pins."
+    )
+    teeth = models.IntegerField(help_text="Number of teeth the sprocket has.")
+
+
+class SprocketProduction(models.Model):
+    actual = models.IntegerField(help_text="Number of sprockets produced.")
+    goal = models.IntegerField(help_text="Number of sprockets to produce.")
+    datetime_produced = models.DateTimeField(
+        help_text="Date and time when sprockets were produced."
+    )
+    factory = models.ForeignKey(Factory, on_delete=models.CASCADE)
+    sprocket = models.ForeignKey(Sprocket, on_delete=models.CASCADE)
